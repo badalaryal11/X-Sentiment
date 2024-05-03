e65836d5ba3c143c4955484d7dd217d358c6bf27
Badal Aryal <badal.aryal@gmail.com>
2024-05-03 13:44:37 +0545

    Set up the twitter framework.

--- X-Sentiment/ViewController.swift
+++ X-Sentiment/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
@@ -16,9 +17,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    let swifter = Swifter(consumerKey: "rMqIfXmYxTiI3ZUnyydL5xWrk", consumerSecret: "PTIAO3p6ltw7JcMHTTDsqqbhZFzmwjzUI9U7ekaq8nPCj3xU9R")
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        swifter.searchTweet(using: "@Apple", success: { results, metadata in
+            print(results)
+        }) {(error) in
+            print("There was an error with the twitter API Request, \(error)")
+        }
     }
 
 

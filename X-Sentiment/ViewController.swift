e465fe60a595b09c6c9993d31edee1fa6fc1fbd4
Badal Aryal <badal.aryal@gmail.com>
2024-05-03 15:33:39 +0545

    Perform Sentiment Classification on the data by Using X-SentimentClassifer.mlmodel

--- X-Sentiment/ViewController.swift
+++ X-Sentiment/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
     
@@ -17,14 +18,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var sentimentLabel: UILabel!
     
+    let sentimentClassifier = X_SentimentClassifier()
+    
     let swifter = Swifter(consumerKey: "rMqIfXmYxTiI3ZUnyydL5xWrk", consumerSecret: "PTIAO3p6ltw7JcMHTTDsqqbhZFzmwjzUI9U7ekaq8nPCj3xU9R")
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+      let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best Company.")
+        print(prediction.label)
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { results, metadata in
-            print(results)
+            //print(results)
         }) {(error) in
             print("There was an error with the twitter API Request, \(error)")
         }
